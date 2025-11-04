15cf456d1d88d14cb8efadda40bce0cd1447184b
sokunbi farid <sokunbifarid1@gmail.com>
2025-11-04 17:54:54 +0100

    update

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from Flask import flask, render_template
+from flask import Flask, render_template
 import sqlite3
-from helper import open_database, close_database, commit_database
+from helper import open_database, close_database, comit_database
 
 app = Flask(__name__)
 
+CATEGORIES = ["Normal", "Important", "Urgent"]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", categories=CATEGORIES)
 
 @app.route("/update")
 def update():
@@ -18,4 +20,17 @@ def delete():
 
 @app.route("/write")
 def write():
+    username = "DEFAULT"
+    todo_name = request.form.get("todo_name")
+    category = request.form.get("category")
+    description = request.form.get("description")
+    if not todo_name or not category or not description:
+        render_template("apology.html")
+    cursor = open_database()
+    try:
+        cursor.execute("INSERT INTO todo_data (username, todo_name, category, description) VALUES(?,?,?,?)", (username, todo_name, category, description))
+        comit_database()
+        close_database()
+    except ValueError as e:
+        render_template("apology.html")
     return 0
---
+++ static/script.js
@@ -0,0 +1,4 @@
+function confirm_delete(id){
+	window.location.href="/index";
+	console.log("function called");
+}
